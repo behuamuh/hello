c888f705f47cf8c95a0c3a2aa1f69da3aea6b918
Behuamuh <sven1980@list.ru>
2018-05-31 16:33:10 +0300

    Adding SearchInText task.

--- src/SearchInText.java
+++ src/SearchInText.java
@@ -1,64 +1,42 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class SearchInText {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Searcher searcher = new Searcher(scanner.next(), scanner.next());
-        searcher.search();
+        search(scanner.next(), scanner.next());
     }
 
-}
-class Searcher{
-    private String text;
-    private String pattern;
-    private int p = 84673;
-    private final int hashP;
-
-    public Searcher(String pattern, String text) {
-        this.text = text;
-        this.pattern = pattern;
-        loadPower();
-        hashP = hash(pattern);
-    }
-
-    private Map<Integer, Integer> power = new HashMap<>();
-    public void search(){
+    public static void search(String pattern, String text){
         if (pattern.length() >= text.length()) {
             if (text.equals(pattern))
                 System.out.println(0);
             return;
         }
+        int hashP = hash(pattern);
         int tempHash = hash(text.substring(0, pattern.length()));
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i <= text.length() - pattern.length(); i++){
+            //System.out.println(tempHash);
             if (tempHash == hashP){
-                if (text.substring(i, i + pattern.length()).equals(pattern))
-                    System.out.print(i + " ");
+                if (text.charAt(i) == pattern.charAt(0))
+                    if (text.substring(i, i + pattern.length()).equals(pattern))
+                        builder.append(i + " ");
             }
             if (i == text.length() - pattern.length())
                 break;
-            int delta = text.charAt(i + pattern.length()) * power.get(pattern.length() - 1);
-            tempHash = delta + (tempHash - text.charAt(i))/ 263;
-            tempHash = tempHash % p;
+            int delta = text.charAt(i + pattern.length());
+            tempHash = delta + (tempHash - text.charAt(i));
         }
-
+        System.out.println(builder.toString());
 
     }
-    private int hash(String s){
+    private static int hash(String s){
         if (s == null || s.length() == 0)
             throw new IllegalArgumentException();
         int sum = 0;
-        for (int i = 0; i < s.length(); i++){
-            sum = (sum + ((int)s.charAt(i) * power.get(i)) % p) % p;
+        for (char c : s.toCharArray()) {
+            sum += (int)c;
         }
         return sum;
-    }
-    private void loadPower(){
-        int x = 1;
-        for (int i = 0; i < pattern.length(); i++){
-            power.put(i, x);
-            x = (x * 263) % p;
-        }
     }
 }
